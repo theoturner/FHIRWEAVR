a886e139aae5a2ec55e7cae46842a689a4ed664e
Theo <10600558+theoturner@users.noreply.github.com>
2018-01-22 16:52:49 +0000

    Moved to GitHub for repo management

---
+++ Assets/createFHIRDoc.cs
@@ -0,0 +1,117 @@
+﻿using System.Xml;
+using UnityEngine;
+//TODO0-4 !!!!!!!!!!!!!!!!!!!!!!!!!
+
+// ================================================= RUN BEFORE LINKING getData to see document generates correctly
+// VERIFY WITH SOMEONE THAT YOU HAVE ALL REQUIRED ITEMS
+
+//TODO4 Find out how to overwrite old XML entries for continuously updating (current) metrics ==========
+
+class createFHIRDoc
+{
+
+    //TODO1 create method to read in from getData and put in a format able to be written by createFHIRDoc. Maybe class instead?
+
+
+    public static void Main() // put arguments in if you want them
+    {
+        //TODO3 Read in some timestamp info and change file name based on it ==========
+        XmlWriter xw = XmlWriter.Create("results.xml");
+        int i;
+        string[] identifiers = { "distance", "speed", "resistance", "heartrate", "rotation", "lean", "incline" };
+        
+        //TODO0 create document start information (before List start)
+
+        xw.WriteStartDocument();
+
+        xw.WriteStartElement("Bundle", "http://hl7.org/fhir");
+        //xw.WriteAttributeString("xmlns", "http://hl7.org/fhir");
+
+        xw.WriteStartElement("meta");
+        xw.WriteStartElement("lastUpdated");
+        xw.WriteAttributeString("value", "[DATETIME]"); //TODO3 read in ==========
+        xw.WriteEndElement();
+        xw.WriteEndElement();
+
+        xw.WriteStartElement("identifier");
+        xw.WriteString("FHIRWEAVR");
+        xw.WriteEndElement();
+
+        xw.WriteStartElement("type");
+        xw.WriteAttributeString("value", "document");
+        xw.WriteEndElement(); // Can we make this a single-line end tag? i.e. />
+
+        xw.WriteStartElement("entry");
+        xw.WriteStartElement("resource");
+        xw.WriteStartElement("Composition");
+
+        xw.WriteStartElement("status");
+        xw.WriteAttributeString("value", "final"); //TODO3 change to 'amended' if changed
+        xw.WriteEndElement();
+
+        xw.WriteStartElement("type");
+        xw.WriteStartElement("coding");
+        xw.WriteStartElement("system");
+        xw.WriteAttributeString("value", "http://loinc.org");
+        // Do we need a 'code' sub-element of 'coding'? See FHIR document example.
+        xw.WriteEndElement();
+        xw.WriteEndElement();
+        xw.WriteStartElement("text");
+        xw.WriteAttributeString("value", "VirZOOM output");
+        xw.WriteEndElement();
+        xw.WriteEndElement();
+
+        xw.WriteStartElement("status");
+        xw.WriteAttributeString("value", "final"); //TODO3 change to 'amended' if changed
+        xw.WriteEndElement();
+
+        xw.WriteStartElement("subject");
+        xw.WriteString("Reference(Device)"); //TODO2 read how to create reference and create a Device record for the VirZOOM ==========
+        xw.WriteEndElement();
+
+        xw.WriteStartElement("date");
+        xw.WriteAttributeString("value", "[dateTime]"); //TODO2 read in date + time and give in exact FHIR format ==========
+        xw.WriteEndElement();
+
+        xw.WriteStartElement("author");
+        xw.WriteString("Reference(Device)"); //TODO2 read how to create reference and create a Device record for the VirZOOM ==========
+        xw.WriteEndElement();
+
+        xw.WriteStartElement("title");
+        xw.WriteString("VirZOOM output");
+        xw.WriteEndElement();
+
+
+        // FHIR List definitions
+        xw.WriteStartElement("List");
+        //xw.WriteAttributeString("xmlns", "http://hl7.org/fhir"); Don't think we need this if it's defined for the bundle
+
+        xw.WriteStartElement("status");
+        xw.WriteAttributeString("value", "current"); //TODO3 overwrite to 'retired' if new document made ==========
+        xw.WriteEndElement();
+
+        xw.WriteStartElement("mode");
+        xw.WriteAttributeString("value", "working"); //TODO3 change to 'snapshot' when exporting halfway through session or new document made ==========
+        xw.WriteEndElement();
+
+        ///////////////////////////
+        //TODO1 read in the below//
+        ///////////////////////////
+        // FHIR List entries
+        for (i = 0; i < 7; i++)
+        {
+            xw.WriteStartElement("entry");
+            xw.WriteStartElement("item");
+            xw.WriteAttributeString(identifiers[i], "[VALUE]"); //TODO1 Read in value
+            xw.WriteEndElement();
+            xw.WriteEndElement();
+        }
+
+        xw.WriteEndElement();
+        xw.WriteEndElement();
+        xw.WriteEndElement();
+
+        xw.WriteEndDocument();
+        xw.Close();
+    }
+}
---
+++ Assets/getData.cs
@@ -0,0 +1,148 @@
+﻿// Scripts for getting both current and session metrics and datasets.
+
+using UnityEngine;
+
+// LATER: go directly to bike without SDK using mBikeState
+
+public class getData : MonoBehaviour
+{
+    private VZController controller;
+    private double distance, speed, resistance, heartrate, rotation, lean, incline;
+    private double[] current, session;
+
+    // Initialization
+    void Start()
+    {
+        createFHIRDoc.Main();
+        controller = VZPlayer.Controller;
+        current = new double[7];
+        session = new double[7];
+    }
+
+    // Called once every frame
+    void Update()
+    {
+        int i;
+        distance = controller.Distance;
+        current[0] = distance;
+        speed = controller.InputSpeed;
+        current[1] = speed;
+        resistance = controller.UncalibratedResistance();
+        current[2] = resistance;
+        heartrate = controller.HeartRate();
+        current[3] = heartrate;
+        rotation = controller.HeadRot;
+        current[4] = rotation;
+        lean = controller.HeadLean; // ENSURE THESE TWO ARE THE RIGHT WAY ROUND
+        current[5] = rotation;
+        incline = controller.HeadBend; // ENSURE THESE TWO ARE THE RIGHT WAY ROUND
+        current[6] = incline;
+        session[0] = distance; // Distance is already cumulative
+        for (i = 1; i < 7; i++) // Consequently, start at index 1
+        {
+            session[i] += current[i];
+        }
+    }
+
+    // Get all data, current/session specified by parameter - after Update()
+    public double[] getAllData(string type)
+    {
+        if (type == "current")
+        {
+            return current;
+        }
+        if (type == "session")
+        {
+            return session;
+        }
+        else
+        {
+            Debug.Log("That type does not exist. Types are 'current' and 'session.'");
+            return new double[7]; // Defaults to 0 for all metrics
+        }
+    }
+
+    // Get a metric, current/session specified by parameter - after Update()
+    // Test on resistance and heart rate - they may not be doubles ===============================================
+    public double getMetric(string metric, string type)
+    {
+        if (type == "current")
+        {
+            if (metric == "distance")
+            {
+                return distance;
+            }
+            if (metric == "speed")
+            {
+                return speed;
+            }
+            if (metric == "resistance")
+            {
+                return resistance;
+            }
+            if (metric == "heartrate")
+            {
+                return heartrate;
+            }
+            if (metric == "rotation")
+            {
+                return rotation;
+            }
+            if (metric == "lean")
+            {
+                return lean;
+            }
+            if (metric == "incline")
+            {
+                return incline;
+            }
+            else
+            {
+                Debug.Log("That metric does not exist. Metrics are 'distance,' 'speed,' 'resistance,' 'heartrate,' 'rotation,' 'lean' and 'incline.'");
+                return 0;
+            }
+        }
+        if (type == "session")
+        {
+            double i = Time.frameCount;
+            if (metric == "distance")
+            {
+                return distance;
+            }
+            if (metric == "speed")
+            {
+                return session[1] / i;
+            }
+            if (metric == "resistance")
+            {
+                return session[2] / i;
+            }
+            if (metric == "heartrate")
+            {
+                return session[3] / i;
+            }
+            if (metric == "rotation")
+            {
+                return session[4] / i;
+            }
+            if (metric == "lean")
+            {
+                return session[5] / i;
+            }
+            if (metric == "incline")
+            {
+                return session[6] / i;
+            }
+            else
+            {
+                Debug.Log("That metric does not exist. Metrics are 'distance,' 'speed,' 'resistance,' 'heartrate,' 'rotation,' 'lean' and 'incline.'");
+                return 0;
+            }
+        }
+        else
+        {
+            Debug.Log("That type does not exist. Types are 'current' and 'session.'");
+            return 0;
+        }
+    }
+}
