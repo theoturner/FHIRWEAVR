41a9b83555793f842569dd509401da70fcdc687a
Theo <10600558+theoturner@users.noreply.github.com>
2018-02-09 15:15:45 +0000

    Finalised PushData as WebClient methods

--- Assets/PushData.cs
+++ Assets/PushData.cs
@@ -1,36 +1,84 @@
 ﻿// Sends data not yet on server to server via FTP.
 
-//TODO MAKE THIS WORK. MAKE SURE YOU PUSH DEVICE PROFILE ALSO!!!! CHECK IF DEVICE PROFILE EXISTS ON SERVER BEFORE SENDING.
+//TODO TEST THIS. ESPECIALLY TEST SENDING TO NONEXISTENT HOST TO SEE IF WEBCLIENT HANDLES ERROR AUTOMATICALLY, IF NOT ADD ERROR HANDLING.
 
-using UnityEngine;
-using System;
-using System.Net;
 using System.IO;
+using System.Linq;
+using System.Net;
+using UnityEngine;
 
 public class PushData
 {
 
-    public void FTPUpload(string fileToUpload, string host, string username, string password)
+    public void Upload(string fileToUpload, string host)
     {
-        string path = DataHandler.path + fileToUpload;
+        // Note that HTTP allows additional / characters so don't need to check final character of host
+        if (!FileExistsAtURL(host + "/VirZOOM-device-profile.xml"))
+        {
+            UploadViaClient(host, DataHandler.path + "VirZOOM-device-profile.xml");
+        }
 
-        WebClient client = new System.Net.WebClient();
-        Uri uri = new Uri(host + new FileInfo(path).Name);
+        // For 'last' keyword as first parameter to push last saved file
+        if (fileToUpload == "last")
+        {
+            var folder = new DirectoryInfo(DataHandler.path);
+            var lastFile = folder.GetFiles().OrderByDescending(f => f.LastWriteTime).First();
+            fileToUpload = lastFile.Name;
+        }
+        
+        string fullFilePath = DataHandler.path + fileToUpload;
+        if (!(File.Exists(fullFilePath)))
+        {
+            Debug.Log("File not found. Please check the filename or use ManualUpload(string fileToUpload, string filePath, string host) for files in non-default save locations.");
+        }
 
-        client.UploadProgressChanged += new UploadProgressChangedEventHandler(Progress);
-        client.UploadFileCompleted += new UploadFileCompletedEventHandler(Complete);
-        client.Credentials = new System.Net.NetworkCredential(username, password);
-        client.UploadFileAsync(uri, "STOR", path);
+        UploadViaClient(host, fileToUpload);
     }
 
-    void Progress(object sender, UploadProgressChangedEventArgs e)
+    public void ManualUpload(string fileToUpload, string filePath, string host)
     {
-        Debug.Log("Uploading: " + e.ProgressPercentage);
+        char lastPathChar = filePath[filePath.Length - 1];
+        // Note that DOS uses / and \ interchangeably, even allowing both in a single path
+        if (lastPathChar != '/' || lastPathChar != '\\')
+        {
+            filePath = filePath + '/';
+        }
+        string fullFilePath = filePath + fileToUpload;
+        if (!(File.Exists(fullFilePath)))
+        {
+            Debug.Log("File not found. Please check that the filename and path.");
+        } 
+        UploadViaClient(host, fullFilePath);
+
+        
     }
 
-    void Complete(object sender, UploadFileCompletedEventArgs e)
+    void UploadViaClient(string host, string fileToUpload)
     {
-        Debug.Log("Uploaded");
+        WebClient FHIRClient = new WebClient();
+        using(FHIRClient)
+        {
+            FHIRClient.UploadFile(host, fileToUpload);
+        }
     }
+
+    public bool FileExistsAtURL(string url)
+    {
+        try
+        {
+            HttpWebRequest request = WebRequest.Create(url) as HttpWebRequest;
+            request.Method = "HEAD";
+            // If HTTP status is 200, file exists
+            HttpWebResponse response = request.GetResponse() as HttpWebResponse;
+            response.Close();
+            return (response.StatusCode == HttpStatusCode.OK);
+        }
+        catch
+        {
+            Debug.Log("Could not detect whether certain files exist on the server. Ensure that you push the device profile.");
+            return false;
+        }
+    }
+
 
 }
