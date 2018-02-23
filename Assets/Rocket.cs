49a25cb3ca9bf32118fa5f51ddd56a34fe4d8995
Theo <10600558+theoturner@users.noreply.github.com>
2018-02-23 14:03:54 +0000

    Anti-physics collisions incomplete

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -2,34 +2,41 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Rocket : MonoBehaviour {
+public class Rocket : MonoBehaviour
+{
 
     Vector3 location;
     DataHandler myDataInstance;
 
-    void Start ()
+    void Start()
     {
 
         myDataInstance = DataHandler.Instance;
         location = transform.position;
 
     }
-	
-	void Update ()
-    {
 
-        location.x = -100 * (float)myDataInstance.GetMetric("lean", "current");
-        location.y = 100 * (float)myDataInstance.GetMetric("incline", "current") + 15;
+    void Update()
+    {
+        location.z += (float)-0.05;
+        //location.x = -100 * (float)myDataInstance.GetMetric("lean", "current");
+        //location.y = 100 * (float)myDataInstance.GetMetric("incline", "current") + 15;
         transform.position = location;
-        
+
     }
+
 
     void OnCollisionEnter(Collision col)
     {
-        if (col.gameObject.name == "Top" || col.gameObject.name == "Bottom")
+        if (col.gameObject.tag == "VZPlayer")
+        {
+            Physics.IgnoreCollision(col.gameObject.GetComponent<Collider>(), collider);
+        }
+
+        if (col.gameObject.name == "Camera")
         {
             Debug.Log("Collision!");
         }
-    }
 
+    }
 }
